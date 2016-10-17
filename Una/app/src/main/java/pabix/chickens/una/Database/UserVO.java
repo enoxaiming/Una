8ba35908e5aa48223fac6232cb004aa9261f0cd8
HaJunHyeok <pawn@boxbreakers.co>
2016-10-17 21:31:35 +0900

    Move UserVO

---
+++ Una/app/src/main/java/pabix/chickens/una/Database/UserVO.java
@@ -0,0 +1,31 @@
+package pabix.chickens.una.Database;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by JunHyeok on 2016. 10. 10..
+ */
+
+public class UserVO extends RealmObject {
+
+    @PrimaryKey
+    private String name;
+    private String token;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
