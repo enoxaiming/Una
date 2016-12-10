fffdeefccc75656fc5f6af51bbe9204928ab3769
HaJunHyeok <pawn@boxbreakers.co>
2016-12-11 00:40:16 +0900

    Project Add page + resources

---
+++ Una/app/src/main/java/pabix/chickens/una/Activity/ProjectAddActivity.java
@@ -0,0 +1,13 @@
+package pabix.chickens.una.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_add);
+    }
+}
